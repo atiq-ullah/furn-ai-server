9dfe09bd099f76a8e09432e62d26585df20a5f6d
Atiq Ullah <sultan.ullah1@icloud.com>
2023-11-24 09:53:59 -0800

    Refactored signalling code

--- task_ai/app/handlers/__init__.py
+++ task_ai/app/handlers/__init__.py
@@ -16,10 +16,7 @@ from .helpers import (
 )
 
 from .setup_signals import (
-    create_exchange,
-    create_queue,
-    bind_queue_to_exchange,
-    publish_message,
+    SignalConnection
 )
 
 import pika
@@ -34,6 +31,7 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 
 
 logger = logging.getLogger(__name__)
+conn = SignalConnection()
 
 
 def post_prompt_handler(request: HttpRequest):
@@ -85,15 +83,6 @@ def get_prompt_handler(request: HttpRequest):
 
 @app.task(soft_time_limit=30)  # type: ignore
 def periodically_check_run_status(p_type: str, run_id: str):
-
-    connection_params = pika.URLParameters("amqp://guest:guest@rabbitmq:5672/")
-
-    # Establish a connection to RabbitMQ
-    connection = pika.BlockingConnection(connection_params)
-
-    # Create a channel
-    channel = connection.channel()
-    
     while True:
         try:
             time.sleep(5)
@@ -102,10 +91,6 @@ def periodically_check_run_status(p_type: str, run_id: str):
             )
 
             if run.status == "completed":
-                # Customize exchange, queue, and routing key names as needed
-                exchange_name = "my_exchange"
-                queue_name = "my_queue"
-                routing_key = "my_routing_key"
 
                 last_message = (
                     client.beta.threads.messages.list(thread_id=promptTypeMap[p_type])  # type: ignore
@@ -113,7 +98,7 @@ def periodically_check_run_status(p_type: str, run_id: str):
                     .content[0]
                 )
                 print(last_message.text.value)  # type: ignore
-                publish_message(channel, exchange_name, routing_key, last_message.text.value)
+                conn.publish_message(last_message.text.value) # type: ignore
                 # TODO: Signal another function here to send response on socket
                 break
         except Exception as e:  # pylint: disable=broad-except
--- task_ai/app/handlers/setup_signals.py
+++ task_ai/app/handlers/setup_signals.py
@@ -1,90 +1,84 @@
 import pika
 
 
-def connect_to_rabbitmq():
-    try:
-        credentials = pika.PlainCredentials("guest", "guest")
-        connection = pika.BlockingConnection(
-            pika.ConnectionParameters("0.0.0.0", 5672, "/", credentials)
-        )
-        return connection
-    except Exception as e:
-        print(f"Error connecting to RabbitMQ: {e}")
-        return None
+class SignalConnection:
+    def __init__(self):
+
+        self.credentials = pika.PlainCredentials("guest", "guest")
+        self.queue_name = "process_prompts_queue"
+        self.exchange_name = "process_prompts"
+        self.routing_key = "process_prompts_routing_key"
+        self.connection_address = "0.0.0.0"
+        self.connection_port = 5672
 
 
-def create_exchange(channel, exchange_name):
-    try:
-        channel.exchange_declare(exchange=exchange_name, exchange_type="direct")
-        print(f"Created exchange: {exchange_name}")
-    except Exception as e:
-        print(f"Error creating exchange: {e}")
+        self.connection = self.connect_to_rabbitmq()
+        print(f"Connected to RabbitMQ: {self.connection}")
+        self.channel = self.connection.channel()
 
+    def connect_to_rabbitmq(self):
+        try:
+            connection_obj = pika.BlockingConnection(
+                pika.ConnectionParameters(
+                    self.connection_address, self.connection_port, "/", self.credentials
+                )
+            )
+            return connection_obj
+        except Exception as e:  # pylint: disable=broad-except
+            print(f"Error connecting to RabbitMQ: {e}")
+            return None
 
-def create_queue(channel, queue_name):
-    try:
-        channel.queue_declare(queue=queue_name)
-        print(f"Created queue: {queue_name}")
-    except Exception as e:
-        print(f"Error creating queue: {e}")
+    def create_exchange(self):
+        try:
+            self.channel.exchange_declare(
+                exchange=self.exchange_name, exchange_type="direct"
+            )
+            print(f"Created exchange: {self.exchange_name}")
+        except Exception as e:  # pylint: disable=broad-except
+            print(f"Error creating exchange: {e}")
 
+    def create_queue(self):
+        try:
+            self.channel.queue_declare(queue=self.queue_name)
+            print(f"Created queue: {self.queue_name}")
+        except Exception as e: # pylint: disable=broad-except
+            print(f"Error creating queue: {e}")
 
-def bind_queue_to_exchange(channel, exchange_name, queue_name, routing_key):
-    try:
-        channel.queue_bind(
-            exchange=exchange_name, queue=queue_name, routing_key=routing_key
-        )
-        print(
-            f"Bound queue {queue_name} to exchange {exchange_name} with routing key {routing_key}"
-        )
-    except Exception as e:
-        print(f"Error binding queue to exchange: {e}")
+    def bind_queue_to_exchange(self):
+        try:
+            self.channel.queue_bind(
+                exchange=self.exchange_name,
+                queue=self.queue_name,
+                routing_key=self.routing_key,
+            )
+            print(
+                f"Bound queue {self.queue_name} to exchange {self.exchange_name} with routing key {self.routing_key}"
+            )
+        except Exception as e:  # pylint: disable=broad-except
+            print(f"Error binding queue to exchange: {e}")
 
+    def publish_message(self, message):
+        try:
+            self.channel.basic_publish(
+                exchange=self.exchange_name, routing_key=self.routing_key, body=message
+            )
+            print(f"Published message: {message}")
+        except Exception as e:  # pylint: disable=broad-except
+            print(f"Error publishing message: {e}")
 
-def publish_message(channel, exchange_name, routing_key, message):
-    try:
-        channel.basic_publish(
-            exchange=exchange_name, routing_key=routing_key, body=message
-        )
-        print(f"Published message: {message}")
-    except Exception as e:
-        print(f"Error publishing message: {e}")
+    def consume_message(self):
+        def callback(ch, method, properties, body): # pylint: disable=unused-argument
+            message = body.decode("utf-8")
+            print(f"Received message: {message}")
+            # Process the message here
 
-
-def consume_message(channel, queue_name):
-    def callback(ch, method, properties, body):
-        message = body.decode("utf-8")
-        print(f"Received message: {message}")
-        # Process the message here
-
-    try:
-        channel.basic_consume(
-            queue=queue_name, on_message_callback=callback, auto_ack=True
-        )
-        print(f"Waiting for messages in the queue {queue_name}. To exit, press Ctrl+C.")
-        channel.start_consuming()
-    except Exception as e:
-        print(f"Error consuming messages: {e}")
-
-
-if __name__ == "__main__":
-    connection = connect_to_rabbitmq()
-    if connection:
-        channel = connection.channel()
-
-        # Customize exchange, queue, and routing key names as needed
-        queue_name = "my_queue"
-        exchange_name = "my_exchange"
-        routing_key = "my_routing_key"
-        create_exchange(channel, exchange_name)
-        create_queue(channel, queue_name)
-        bind_queue_to_exchange(channel, exchange_name, queue_name, routing_key)
-
-        # Example usage: Publish a message
-        publish_message(channel, exchange_name, routing_key, 'Hello, RabbitMQ!')
-
-
-        publish_message(channel, exchange_name, routing_key, 'Hello, Again!')
-
-        # Start consuming messages
-        consume_message(channel, queue_name)
+        try:
+            self.channel.basic_consume(
+                queue=self.queue_name, on_message_callback=callback, auto_ack=True
+            )
+            print(
+                f"Waiting for messages in the queue {self.queue_name}. To exit, press Ctrl+C."
+            )
+            self.channel.start_consuming()
+        except Exception as e:  # pylint: disable=broad-except
+            print(f"Error consuming messages: {e}")
