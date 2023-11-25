5fcdde753d2ad61a274b4ff1d4326ce3689fd67b
Atiq Ullah <sultan.ullah1@icloud.com>
2023-11-25 12:28:04 -0800

    Fixed docker image

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+# Function to check if RabbitMQ is up
+wait_for_rabbitmq() {
+    echo "Waiting for RabbitMQ to start..."
+    while ! nc -z rabbitmq 5672; do
+      sleep 1
+    done
+    echo "RabbitMQ started"
+}
 
+# Call the function to wait for RabbitMQ
+wait_for_rabbitmq
 python manage.py makemigrations app
 python manage.py migrate
 
